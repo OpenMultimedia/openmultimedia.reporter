19f23e908d9624745db9522553b4239d375157a5
Gonzalo Almeida <flecox@gmail.com>
2013-03-17 19:53:34 -0300

    agregando palyers omplayer

--- src/openmultimedia/reporter/static/javascript.js
+++ src/openmultimedia/reporter/static/javascript.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function() {
 
+    var slug = $("#new-report-video").attr("data-slug");
+    $("#new-report-video").omplayer({
+        slug: slug,
+        width: 400, height: 255,
+        style: 1
+    });
+
     $("#search-ireport-button").click(function(e) {
         e.preventDefault();
         var search = $(".search-report-search").val();
@@ -39,25 +46,33 @@ $(document).ready(function() {
         //and then the html generated i the iframe is pasted in the real dom
         //we do this because there's some javascript that needs to be executed
         //in order
-        //var url = $(this).attr("data-url");
-        var url = $(this).attr("data-url") + "/ajax-report";
+        var url = $(this).attr("data-url");
+        // var url = $(this).attr("data-url") + "/ajax-report";
         $(".main-report").children().css("display", "none");
         $("#loading").css("display", "block");
-        var iframe = document.createElement("iframe");
-        iframe.src = url;
-        $(".hidden-frame").remove();
-        $(iframe).attr("class","hidden-frame");
-        $(iframe).css("display","none");
-        document.body.appendChild(iframe);
-        $(".hidden-frame").load(function() {
-            $(".main-report").html($(".hidden-frame").contents().find("html").html());
-            $(".hidden-frame").remove();
-        });
-        // $.ajax({
-        //     url: url + "/ajax-report",
-        //     success:function(data) {
-        //         $(".main-report").html(data);            }
+        // var iframe = document.createElement("iframe");
+        // iframe.src = url;
+        // $(".hidden-frame").remove();
+        // $(iframe).attr("class","hidden-frame");
+        // $(iframe).css("display","none");
+        // document.body.appendChild(iframe);
+        // $(".hidden-frame").load(function() {
+        //     $(".main-report").html($(".hidden-frame").contents().find("html").html());
+        //     $(".hidden-frame").remove();
         // });
+        $.ajax({
+            url: url + "/ajax-report",
+            success:function(data) {
+                $(".main-report").html(data);
+                var slug = $("#new-report-video").attr("data-slug");
+                $("#new-report-video").omplayer({
+                    slug: slug,
+                    width: 400, height: 255,
+                    style: 1
+                });
+
+            }
+        });
     });
 
     var pairs = $(".report-pair");
